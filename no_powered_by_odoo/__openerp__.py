178e8d0fa33eb7ad768df7927a457abbc0d526a9
Marc-Etienne Leblanc <marc_e_leblanc@outlook.com>
2015-05-28 11:05:13 -0400

    module submit for review

---
+++ no_powered_by_odoo/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "no_powered_by_odoo",
+    'version': '1.0',
+    'category' : 'demo',
+    'summary': """
+        Removes "Powered by Odoo" branding.""",
+    'description': """
+        Removes "Powered by Odoo" branding found on login screen and main app page.""",
+    'author': "Microcom",
+    'images': [],
+    'depends': ['web'],
+    'demo': [],
+    'test': [],
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+    'auto_install': False,
+    'installable': True,
+}
