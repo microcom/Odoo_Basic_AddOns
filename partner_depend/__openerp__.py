a6ea24f707dc95952d1413bdcfb1fce834c96886
Stephane Le Cornec <stephane.lecornec@gmail.com>
2015-06-22 13:53:12 -0400

    partner_depend

---
+++ partner_depend/__openerp__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'partner_depend',
+    'version': '1.0',
+    'category' : 'demo',
+    'description': """
+Demo fields.
+============
+
+It demos fields.
+    """,
+    'author': 'Microcom',
+    'images': [],
+    'depends': ['base'],
+    'demo': [],
+    'test': [],
+    'data': [
+        'res_partner_view.xml',
+    ],
+    'auto_install': False,
+    'installable': True,
+}
---
+++ partner_depend/res_partner.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+from openerp import models, fields, api
+
+
+class res_partner(models.Model):
+    _inherit = "res.partner"
+
+    @api.one
+    @api.onchange('test_master')
+    def onchange_test(self):
+        self.test_change = self.test_master
+
+    @api.one
+    @api.depends('test_master')
+    def _compute_master(self):
+        self.depend_master = self.test_master
+
+    @api.one
+    # @api.depends('test_change')
+    def _compute_change(self):
+        self.depend_change = self.test_change
+
+    test_master = fields.Char('Master')
+    test_change = fields.Char('Change')
+    depend_master = fields.Char('Depend Master', compute=_compute_master)
+    depend_change = fields.Char('Depend Change', compute=_compute_change)
