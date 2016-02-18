cb25d18de3ed6a8ef499f53f39d91b5c69daaf70
Stephane Le Cornec <stephane.lecornec@gmail.com>
2016-02-18 14:58:40 -0500

    skid_label

---
+++ skid_label/__openerp__.py
@@ -0,0 +1,24 @@
+# noinspection PyStatementEffect
+{
+    'name': 'skid_label',
+    'version': '1.0',
+    'category': 'demo',
+    'description': """
+Demo fields.
+============
+
+It demos fields.
+    """,
+    'author': 'Microcom',
+    'images': [],
+    'depends': ['stock'],
+    'demo': [],
+    'test': [],
+    'data': [
+        'data/stock_picking.xml',
+        'reports/print_skid_label.xml',
+        'views/templates.xml',
+    ],
+    'auto_install': False,
+    'installable': True,
+}
